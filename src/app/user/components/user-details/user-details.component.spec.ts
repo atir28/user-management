050489904e33f496f70c582bd536ab34b875c821
abhishekgiri02 <abhishek.giri@bitskraft.com>
2024-09-15 16:42:50 +0545

    application specs fare updated and it pased the test 'ng-test'

--- src/app/user/components/user-details/user-details.component.spec.ts
+++ src/app/user/components/user-details/user-details.component.spec.ts
@@ -1,21 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserDetailsComponent } from './user-details.component';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { Store } from '@ngrx/store';
 
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
+  let mockStore: MockStore;
+
+  const initialState = { user: { users: [] } }; 
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [UserDetailsComponent]
+      declarations: [UserDetailsComponent],
+      providers: [
+        provideMockStore({ initialState }),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(convertToParamMap({ id: '1' })), 
+            snapshot: {
+              paramMap: convertToParamMap({ id: '1' })
+            }
+          }
+        }
+      ]
     });
+
     fixture = TestBed.createComponent(UserDetailsComponent);
     component = fixture.componentInstance;
+    mockStore = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/user/components/user/user.component.spec.ts
+++ src/app/user/components/user/user.component.spec.ts
@@ -1,17 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserComponent } from './user.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let mockStore: MockStore;
+
+  const initialState = { user: { users: [] } };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [UserComponent]
+      declarations: [UserComponent],
+      imports: [FormsModule, ReactiveFormsModule],
+      providers: [
+        provideMockStore({ initialState }),
+      ]
     });
+
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    mockStore = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
